a7ccf310ee214e0ca57f2e9f99ac94c9a01e2f67
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-08-30 16:49:52 +0530

    this is a dev commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
+import logoImg from '../assets/logo.jpg'
+
 export const Header = () => {
     return (
-        <></>
+        <>
+        <header id="main-header">
+            <div id="title">
+                <img src={logoImg} alt="A restaurant"/>
+                <h1>Eatopia</h1>
+            </div>
+            <nav>
+                <button>Cart (0)</button>
+            </nav>
+        </header>
+        </>
     )
 }
