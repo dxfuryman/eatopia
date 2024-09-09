2c6e22b0fb6eaa0f48b581534a4920ae76b467a8
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-09-10 03:31:51 +0530

    this is a dev commit

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,22 @@
+import {createPortal} from "react-dom";
+import {useEffect, useRef} from "react";
+
+export const Modal = ({children, open, className = ''}) => {
+    const dialog = useRef();
+    
+    useEffect(() => {
+        if (open){
+            dialog.current.showModal();
+        }
+    },[open]);
+
+
+    return (
+        createPortal(
+            <dialog ref={dialog} className={`modal ${className}`}>
+                {children}
+            </dialog>,
+            document.getElementById('modal')
+        )
+    )
+}
