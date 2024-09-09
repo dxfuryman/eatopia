f99576cd4bf6150f6605a1dc2b85280011786421
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-09-09 22:23:46 +0530

    this is a dev commit

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -29,14 +29,46 @@ function cartReducer(state,action){
       return {...state, items: updatedItems};
     }
     if (action.type === 'REMOVE_ITEM'){
-        // ... remove an item from the state
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        const updatedItems = [...state.items];
+
+        if (existingCartItem.quantity === 1){
+            updatedItems.splice(existingCartItemIndex, 1);   //This medhod splice is used to delete the last index in the arary
+        } else {
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity - 1,
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return { ...state, items: updatedItems};
     }
 
     return state;
 }
 export  function CartContextProvider(children){
-    useReducer(cartReducer, {items: []});
-    return<CartContext.Provider>
+   const [cart, dispatchCartAction ] = useReducer(cartReducer, {items: []});
+
+   function addItem(item){
+       dispatchCartAction({type: 'ADD_ITEM', item: item});
+   }
+
+    function removeItem(id){
+        dispatchCartAction({type: 'REMOVE_ITEM', id});
+    }
+
+    const cartContext = {
+        items : cart.items,
+        addItem,
+        removeItem
+    };
+
+   return<CartContext.Provider value={cartContext}>
         {children}
     </CartContext.Provider>
 }
